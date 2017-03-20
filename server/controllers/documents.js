f5af6035d5055a74cea2e6711a1328458bbdfed3
andela-uofoegbu <ugonna.ofoegbu@andela.com>
2017-03-20 12:32:14 +0100

    feature(create-controllers): Add controllers

---
+++ server/controllers/documents.js
@@ -0,0 +1,158 @@
+import { Document, Role } from '../models/index';
+
+const PAGE_LIMIT = 10;
+const PAGE_OFFSET = 0;
+
+const DocumentController = {
+
+  createDocument: (req, res) => {
+    Document.create({
+      title: req.body.title,
+      user_id: req.decoded.id,
+      content: req.body.content,
+      access: req.body.access || 'public'
+    })
+    .then((document, err) => {
+      if (err) {
+        res.status(500).json({ error: err.message });
+      } else {
+        res.status(201).json(document);
+      }
+    })
+    .catch((err) => {
+      res.status(500).json({ error: err.message });
+    });
+  },
+  getDocuments: (req, res) => {
+    const orderDirection = req.query.order || 'DESC';
+    let queryParams = {
+      limit: req.query.limit || PAGE_LIMIT,
+      offset: req.query.offset || PAGE_OFFSET,
+      order: [['publish_date', orderDirection]]
+    };
+    Role.find({
+      where: {
+        id: req.decoded.role_id
+      }
+    })
+    .then((role) => {
+      if (role.title === 'admin' && req.query.roleid) {
+        queryParams = Object.assign({}, queryParams, {
+          where: {
+            role_id: req.query.role_id
+          }
+        });
+      }
+      Document.all(queryParams)
+      .then((documents) => {
+        // eslint-disable-next-line arrow-body-style
+        const returnDocuments = documents.filter((document) => {
+          return (role.title === 'admin' || document.access === 'public' ||
+          (document.access === 'role' && document.role_id === req.decoded.role_id));
+        });
+        return res.status(200).json(returnDocuments.length ? returnDocuments : null);
+      })
+      .catch((err) => {
+        res.status(500).json({ error: err.message });
+      });
+    });
+  },
+  findDocument: (req, res) => {
+    let queryParams = {};
+    Role.find({
+      where: {
+        id: req.decoded.role_id
+      }
+    })
+    .then((role) => {
+      queryParams = {
+        where: {
+          id: req.params.id
+        }
+      };
+      Document.findOne(queryParams).then((document) => {
+        if (role.title === 'admin' || document.access === 'public' ||
+        (document.access === 'role' && document.role_id === req.decoded.role_id)) {
+          return res.status(200).json(document);
+        }
+        return res.status(200).json(null);
+      }).catch((err) => {
+        res.status(400).json({ error: err.message });
+      });
+    });
+  },
+  updateDocumentInfo: (req, res) => {
+    Document.find({
+      where: {
+        id: req.params.id
+      }
+    })
+      .then((document) => {
+        if (document) {
+          document.title = req.body.title;
+          document.content = req.body.content;
+          document.save()
+            .then((err) => {
+              if (err) {
+                return res.status(500).json({ error: err.message });
+              }
+                //  "Document Information Updated"
+              return res.status(200).json(req.body);
+            })
+            .catch((err) => {
+              res.status(500).json({ error: err.message });
+            });
+        }
+      });
+  },
+  deleteDocument: (req, res) => {
+    if (req.decoded) {
+      Document.destroy({
+        where: {
+          id: req.params.id
+        }
+      })
+      .then((document, err) => {
+        if (err) {
+          res.status(500).json({ error: err.message });
+        } else {
+          res.status(200).json(document);
+        }
+      });
+    }
+  },
+  findUserDocuments: (req, res) => {
+    Document.all({
+      where: { user_id: req.params.id }
+    }).then((documents) => {
+      res.status(200).json(documents);
+    })
+      .catch((err) => {
+        res.status(500).json({ error: err.message });
+      });
+  },
+  searchDocuments: (req, res) => {
+    if (req.query.q) {
+      Document.findAll({
+        where: {
+          title: {
+            $iLike: `%${req.query.q}%`
+          }
+        }
+      })
+      .then((documents) => {
+        documents = req.query.publish_date ? documents.filter((document) => {
+
+        }) : documents;
+        res.status(200).json(documents);
+      })
+      .catch((err) => {
+        res.status(500).json({ error: err.message });
+      });
+    } else {
+      res.status(500).json({ error: 'Search query not found' });
+    }
+  }
+};
+
+module.exports = DocumentController;
---
+++ server/controllers/users.js
@@ -0,0 +1,137 @@
+import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
+import { User } from '../models/index';
+
+require('dotenv').config();
+
+
+class UserController {
+
+  static login(req, res) {
+    User.findOne({ where: { email: req.body.email } })
+      .then((user) => {
+        bcrypt.compare(req.body.password, user.password_digest, (err, same) => {
+          const token = jwt.sign({ user }, process.env.SECRET_KEY, { expiresIn: '1h' });
+          res.status(200).json({ success: same, token });
+        });
+      })
+      .catch((err) => {
+        res.status(404).json({ error: err.message });
+      });
+  }
+
+  static createUser(req, res) {
+    User.create({
+      first_name: req.body.first_name,
+      last_name: req.body.last_name,
+      username: req.body.username,
+      email: req.body.email,
+      password: req.body.password,
+      password_confirmation: req.body.password_confirmation,
+      role_id: req.body.role_id
+    })
+      .then((user, err) => {
+        if (err) {
+          res.status(400).json({ error: err.message });
+        } else {
+          res.status(201).json({ user });
+        }
+      })
+      .catch((err) => {
+        res.status(400).json({ error: err.message });
+      });
+  }
+
+  static getUsers(req, res) {
+    let queryParams = {
+      limit: 10,
+      offset: 0
+    };
+    if (req.query.limit && req.query.offset) {
+      queryParams = {
+        limit: req.query.limit,
+        offset: req.query.offset
+      };
+    }
+    User.all(queryParams)
+      .then((users) => {
+        res.status(200).json(users);
+      })
+      .catch((err) => {
+        res.status(500).json({ error: err.message });
+      });
+  }
+
+  static findUser(req, res) {
+    User.findOne({ where: { id: req.params.id } }).then((user) => {
+      res.status(200).json(user);
+    }).catch((err) => {
+      res.status(404).json({ error: err.message });
+    });
+  }
+
+  static updateUserInfo(req, res) {
+    if (req.decoded) {
+      User.find({
+        where: {
+          id: req.decoded.id
+        }
+      })
+      .then((user) => {
+        if (user) {
+          user.name = req.body.name;
+          user.password = req.body.password;
+          user.password_confirmation = req.body.password_confirmation;
+          user.save()
+            .then((err) => {
+              if (err) {
+                return res.status(500).json({ error: err.message });
+              }
+                //  "User Information Updated"
+              return res.status(200).json(req.body);
+            })
+            .catch((err) => {
+              res.status(500).json({ error: err.message });
+            });
+        }
+      });
+    }
+  }
+
+  static deleteUser(req, res) {
+    User.destroy({
+      where: {
+        id: req.params.id
+      }
+    })
+      .then((user, err) => {
+        if (err) {
+          res.status(500).json({ error: err.message });
+        } else {
+          res.status(200, 'User Deleted').json(user);
+        }
+      });
+  }
+
+  static searchUsers(req, res) {
+    if (req.query.q) {
+      User.findAll({
+        where: {
+          name: {
+            $iLike: `%${req.query.q}%`
+          }
+        }
+      })
+      .then((users) => {
+        res.status(200).json(users);
+      })
+      .catch((err) => {
+        res.status(500).json({ error: err.message });
+      });
+    } else {
+      res.status(500).json({ error: 'Search query not found' });
+    }
+  }
+}
+
+module.exports = UserController;
